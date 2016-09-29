058fa278772b9d6b7fee2dc56ef1df134575c0e7
Low Wai Hong <fatsolow88@gmail.com>
2016-09-29 10:13:00 +0800

    Initial Commit

---
+++ Project1_StormViewerTests/Project1_StormViewerTests.swift
@@ -0,0 +1,36 @@
+//
+//  Project1_StormViewerTests.swift
+//  Project1_StormViewerTests
+//
+//  Created by Low Wai Hong on 29/09/2016.
+//  Copyright © 2016 Low Wai Hong. All rights reserved.
+//
+
+import XCTest
+@testable import Project1_StormViewer
+
+class Project1_StormViewerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
